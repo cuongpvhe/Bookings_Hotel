590dee97047b85147b3b97d1df39fcf75741c1aa
Bien Cuong <dogongocky@gmail.com>
2024-09-27 00:18:42 +0700

    up

---
+++ Pages/Shared/Layout_admin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bookings_Hotel.Pages.Shared
+{
+    public class Layout_adminModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
