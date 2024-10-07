101c626e6e1a48ad471dd031d6d9b863911c6d5e
huonglee23 <huonglee160923@gmail.com>
2024-10-07 22:18:49 +0700

    add template

---
+++ Pages/Contact.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bookings_Hotel.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
