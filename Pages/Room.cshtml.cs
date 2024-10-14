2dc137b3bf2111d25199f4c4b91c46810744667a
huonglee23 <huonglee160923@gmail.com>
2024-10-14 20:35:51 +0700

    up filter in room list

--- Pages/Room.cshtml.cs
+++ Pages/Room.cshtml.cs
@@ -9,17 +9,62 @@ namespace Bookings_Hotel.Pages
     {
 
         private readonly HotelBookingSystemContext _context;
-        public List<Room> Rooms { get; set; }
-        public List<Service> Services { get; set; }
-
         public RoomModel(HotelBookingSystemContext context)
         {
             _context = context;
         }
-        public void OnGet()
+
+        [BindProperty(SupportsGet = true)]
+        public decimal? PriceMin { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public decimal? PriceMax { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int? SortPrice { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public List<string> SelectedServices { get; set; } = new List<string>();
+
+        public List<Room> Rooms { get; set; }
+        public List<Service> Services { get; set; }
+
+        public async Task OnGetAsync()
         {
-            Services = _context.Services.ToList();
-            Rooms = _context.Rooms.Include(x => x.Type).Include(x => x.RoomImages).Include(x => x.Reviews).ToList();
+            Services = await _context.Services.ToListAsync();
+
+            var query = _context.Rooms.Include(x => x.Type).Include(x => x.RoomImages).Include(x => x.Reviews).AsQueryable();
+
+            //Filter price
+            if (PriceMin.HasValue)
+            {
+                query = query.Where(x => x.Price >= PriceMin.Value);
+            }
+            else if (PriceMax.HasValue)
+            {
+                query = query.Where(x => x.Price <= PriceMax.Value);
+            }
+
+            //Filter services
+            if (SelectedServices != null && SelectedServices.Count > 0)
+            {
+                query = query.Where(room => room.RoomServices.Any(service => SelectedServices.Contains(service.Service.ServiceName)));
+            }
+
+            //Filter sort price
+            switch (SortPrice)
+            {
+                case 1:
+                    query = query.OrderBy(x => x.Price);
+                    break;
+                case 2:
+                    query = query.OrderByDescending(x => x.Price);
+                    break;
+                default:
+                    SortPrice = 3;
+                    break;
+            }
+            Rooms = await query.ToListAsync();
         }
     }
 }
