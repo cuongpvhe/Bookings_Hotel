eae5db22dd471058c8cb9e5b836a708954d4ddc1
huonglee23 <huonglee160923@gmail.com>
2024-10-22 16:37:17 +0700

    feature/staff/add pagination combine with ajax

--- Pages/Manager/Services.cshtml.cs
+++ Pages/Manager/Services.cshtml.cs
@@ -13,28 +13,50 @@ namespace Bookings_Hotel.Pages.Manager
             _context = context;
         }
 
-        //List
+        // List
         public List<string>? TableHeaders { get; set; } = new List<string> { ".No", "Service Id", "Service Name", "Created Date", "Update Date", "Price", "Actions" };
         public List<Service> Services { get; set; }
 
-        public async Task<IActionResult> OnGetAsync()
+        // Pagination
+        public int CurrentPage { get; set; } = 1;
+        public int TotalPages { get; set; }
+        public const int PageSize = 2;
+
+        public async Task<IActionResult> OnGetAsync(int pageIndex = 1)
         {
-            Services = await _context.Services.ToListAsync();
+            CurrentPage = pageIndex;
+            var servicesQuery = _context.Services.AsQueryable();
+            TotalPages = (int)Math.Ceiling(await servicesQuery.CountAsync() / (double)PageSize);
+
+            Services = await servicesQuery
+                .Skip((CurrentPage - 1) * PageSize)
+                .Take(PageSize)
+                .ToListAsync();
             return Page();
         }
 
-        public async Task<IActionResult> OnGetSearchAsync(string searchTerm)
+        public async Task<IActionResult> OnGetSearchAsync(string searchTerm, int pageIndex = 1)
         {
+            CurrentPage = pageIndex;
+            IQueryable<Service> servicesQuery;
+
             if (string.IsNullOrWhiteSpace(searchTerm))
             {
-                Services = await _context.Services.ToListAsync();
+                servicesQuery = _context.Services;
             }
             else
             {
-                Services = await _context.Services.Where(x => x.ServiceName.Contains(searchTerm)).ToListAsync();
-
+                servicesQuery = _context.Services.Where(x => x.ServiceName.Contains(searchTerm));
             }
+
+            TotalPages = (int)Math.Ceiling(await servicesQuery.CountAsync() / (double)PageSize);
+
+            Services = await servicesQuery
+                .Skip((CurrentPage - 1) * PageSize)
+                .Take(PageSize)
+                .ToListAsync();
             return Partial("PartialViews/Manager/_ServicesPartialView", this);
         }
     }
+
 }
