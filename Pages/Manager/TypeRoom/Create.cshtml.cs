7ae3ed8c46bc96ee9bb506feda725211c991cc9e
Quan Minh Nguyen <quan2272@gmail.com>
2024-10-26 18:47:53 +0700

    update Delete Type room,

--- Pages/Manager/TypeRoom/Create.cshtml.cs
+++ Pages/Manager/TypeRoom/Create.cshtml.cs
@@ -6,39 +6,106 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Bookings_Hotel.Models;
+using CloudinaryDotNet.Actions;
+using CloudinaryDotNet;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bookings_Hotel.Pages.Manager.TypeRoom
 {
     public class CreateModel : PageModel
     {
-        private readonly Bookings_Hotel.Models.HotelBookingSystemContext _context;
 
-        public CreateModel(Bookings_Hotel.Models.HotelBookingSystemContext context)
+        private readonly HotelBookingSystemContext _context;
+        private readonly Cloudinary _cloudinary;
+
+        public List<Models.Service> Services { get; set; }
+
+        public CreateModel(HotelBookingSystemContext context, Cloudinary cloudinary)
         {
             _context = context;
+            _cloudinary = cloudinary;
         }
 
-        public IActionResult OnGet()
+        public async Task<IActionResult> OnGetAsync()
         {
+            Services = await _context.Services.ToListAsync();
             return Page();
         }
 
-        [BindProperty]
-        public Models.TypeRoom TypeRoom { get; set; } = default!;
-        
-
-        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-          if (!ModelState.IsValid || _context.TypeRooms == null || TypeRoom == null)
-            {
-                return Page();
-            }
+            // Lấy dữ liệu từ form
+            var typeName = Request.Form["TypeName"].ToString();
+            var numberOfBeds = int.Parse(Request.Form["NumberOfBeds"]);
+            var numberOfAdults = int.Parse(Request.Form["NumberOfAdults"]);
+            var numberOfChildren = int.Parse(Request.Form["NumberOfChildren"]);
+            var price = decimal.Parse(Request.Form["Price"]);
+            var description = Request.Form["Description"].ToString();
 
-            _context.TypeRooms.Add(TypeRoom);
+            // Lưu thông tin phòng vào bảng Room
+            var typeRoom = new Models.TypeRoom
+            {
+                TypeName = typeName,
+                NumberOfBed = numberOfBeds,
+                NumberOfAdult = numberOfAdults,
+                NumberOfChild = numberOfChildren,
+                Price = price,
+                Deleted = false,
+                Description = description,
+
+            };
+            _context.TypeRooms.Add(typeRoom);
             await _context.SaveChangesAsync();
 
-            return RedirectToPage("./Index");
+            // Lưu danh sách dịch vụ cho phòng vào bảng RoomService
+            var serviceIds = Request.Form["ServiceIds"].ToList();
+
+
+            foreach (var serviceId in serviceIds)
+            {
+                var typeRoomService = new Models.TypeRoomService
+                {
+                    TypeId = typeRoom.TypeId,
+                    ServiceId = int.Parse(serviceId)
+                };
+                _context.TypeRoomServices.Add(typeRoomService);
+
+            }
+            await _context.SaveChangesAsync(); // Lưu RoomService vào DB
+
+
+            // Lưu ảnh vào bảng RoomImage
+            var images = Request.Form.Files;
+            for (int i = 0; i < images.Count; i++)
+            {
+                var file = images[i];
+                var imageIndex = int.Parse(Request.Form[$"Images[{i}][index]"]);
+
+                // Upload ảnh lên Cloudinary
+                var uploadResult = await _cloudinary.UploadAsync(new ImageUploadParams
+                {
+                    File = new FileDescription(file.FileName, file.OpenReadStream()),
+                    Folder = "hotel_images"
+                });
+
+                var typeRoomImage = new TypeRoomImage
+                {
+                    TypeId = typeRoom.TypeId,
+                    ImageUrl = uploadResult.Url.ToString(),
+                    ImageIndex = imageIndex
+                };
+                _context.TypeRoomImages.Add(typeRoomImage);
+            }
+
+            await _context.SaveChangesAsync();
+
+            return new JsonResult(new { success = true });
         }
+
+/*        public async Task<IActionResult> OnPostCheckRoomNumberAsync(int roomNumber)
+        {
+            var roomExists = await _context.Rooms.AnyAsync(r => r.RoomNumber == roomNumber);
+            return new JsonResult(new { exists = roomExists });
+        }*/
     }
 }
---
+++ wwwroot/admin_template/js/addTypeRoom.js
@@ -0,0 +1,382 @@
+﻿$(document).ready(function () {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+})
+
+// image
+let imageIndex = 0;
+const uploadedImages = {};
+
+let cropper;
+let currentImageIndex;
+
+function handleImageUpload(input) {
+    if (input.files && input.files[0]) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            $('#cropImageModal').modal('show');
+            $('#imageToCrop').attr('src', e.target.result);
+            $('#cropImageModal').on('shown.bs.modal', function () {
+                if (cropper) {
+                    cropper.destroy();
+                }
+                cropper = new Cropper(document.getElementById('imageToCrop'), {
+                    aspectRatio: 5 / 3,
+                    viewMode: 1,
+                    autoCropArea: 1,
+                    scalable: true,
+                    zoomable: true,
+                    ready() {
+                        this.cropper.setCropBoxData({
+                            width: this.cropper.getContainerData().width,
+                            height: this.cropper.getContainerData().height,
+                        });
+                    },
+                });
+            });
+            currentImageIndex = imageIndex;
+        };
+        reader.readAsDataURL(input.files[0]);
+        input.value = ''; // Reset input value to allow re-upload of the same file
+    }
+}
+
+$('#cropImageModal').on('hidden.bs.modal', function () {
+    if (cropper) {
+        cropper.destroy();
+    }
+});
+
+$('#cropImageButton').on('click', function () {
+    const canvas = cropper.getCroppedCanvas({
+        width: 1000,
+        height: 600,
+    });
+
+    canvas.toBlob(function (blob) {
+        const url = URL.createObjectURL(blob);
+        if (currentImageIndex === imageIndex) {
+            addImageToCarousel(url);
+            addThumbnail(url);
+        } else {
+            updateImageInCarousel(url, currentImageIndex);
+            updateThumbnail(url, currentImageIndex);
+        }
+        uploadedImages[currentImageIndex] = new File([blob], 'croppedImage.jpg', { type: 'image/jpeg' });
+        $('#cropImageModal').modal('hide');
+    }, 'image/jpeg');
+});
+
+function handleThumbnailChange(input, index) {
+    if (input.files && input.files[0]) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            $('#cropImageModal').modal('show');
+            $('#imageToCrop').attr('src', e.target.result);
+            $('#cropImageModal').on('shown.bs.modal', function () {
+                if (cropper) {
+                    cropper.destroy();
+                }
+                cropper = new Cropper(document.getElementById('imageToCrop'), {
+                    aspectRatio: 5 / 3,
+                    viewMode: 1,
+                    autoCropArea: 1,
+                    scalable: true,
+                    zoomable: true,
+                    ready() {
+                        this.cropper.setCropBoxData({
+                            width: this.cropper.getContainerData().width,
+                            height: this.cropper.getContainerData().height,
+                        });
+                    },
+                });
+            });
+            currentImageIndex = index;
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+function addImageToCarousel(imageSrc) {
+    const carouselInner = document.getElementById('carousel-inner');
+    const div = document.createElement('div');
+    div.className = 'carousel-item' + (imageIndex === 0 ? ' active' : '');
+    div.setAttribute('data-index', imageIndex);
+    div.innerHTML = `<img src="${imageSrc}" class="d-block w-100" alt="Image ${imageIndex}">`;
+    carouselInner.appendChild(div);
+    imageIndex++;
+}
+
+function addThumbnail(imageSrc) {
+    const thumbnailContainer = document.querySelector('.thumbnail-container');
+    const thumbnailItem = document.createElement('div');
+    thumbnailItem.className = 'thumbnail-item';
+    const img = document.createElement('img');
+    img.src = imageSrc;
+    img.alt = `Thumbnail ${imageIndex - 1}`;
+    img.setAttribute('data-index', imageIndex - 1);
+    img.onclick = function () {
+        const carousel = new bootstrap.Carousel(document.getElementById('carouselExample'));
+        carousel.to(parseInt(img.getAttribute('data-index')));
+    };
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.setAttribute('data-index', imageIndex - 1);
+    input.onchange = function () {
+        handleThumbnailChange(this, parseInt(input.getAttribute('data-index')));
+    };
+    const button = document.createElement('button');
+    button.className = 'btn btn-sm btn-primary';
+    button.innerText = 'Change';
+    button.onclick = function () {
+        input.click();
+    };
+
+    thumbnailItem.appendChild(img);
+    thumbnailItem.appendChild(button);
+    thumbnailItem.appendChild(input);
+    thumbnailContainer.appendChild(thumbnailItem);
+}
+
+function updateImageInCarousel(imageSrc, index) {
+    const carouselInner = document.getElementById('carousel-inner');
+    const items = carouselInner.getElementsByClassName('carousel-item');
+    for (let item of items) {
+        if (parseInt(item.getAttribute('data-index')) === index) {
+            item.querySelector('img').src = imageSrc;
+        }
+    }
+}
+
+function updateThumbnail(imageSrc, index) {
+    const thumbnailContainer = document.querySelector('.thumbnail-container');
+    const imgs = thumbnailContainer.getElementsByTagName('img');
+    for (let img of imgs) {
+        if (parseInt(img.getAttribute('data-index')) === index) {
+            img.src = imageSrc;
+        }
+    }
+}
+
+function deleteLastImage() {
+    if (imageIndex > 0) {
+        const carouselInner = document.getElementById('carousel-inner');
+        const thumbnailContainer = document.querySelector('.thumbnail-container');
+
+        carouselInner.removeChild(carouselInner.lastElementChild);
+        thumbnailContainer.removeChild(thumbnailContainer.lastElementChild);
+
+        imageIndex--;
+
+        if (carouselInner.querySelector('.carousel-item.active') === null && imageIndex > 0) {
+            carouselInner.lastElementChild.classList.add('active');
+        }
+    }
+}
+
+document.getElementById('rotateLeft').addEventListener('click', function () {
+    cropper.rotate(-45);
+});
+
+document.getElementById('rotateRight').addEventListener('click', function () {
+    cropper.rotate(45);
+});
+
+document.getElementById('scaleX').addEventListener('click', function () {
+    const currentScaleX = cropper.getData().scaleX || 1;
+    cropper.scaleX(-currentScaleX);
+});
+
+document.getElementById('scaleY').addEventListener('click', function () {
+    const currentScaleY = cropper.getData().scaleY || 1;
+    cropper.scaleY(-currentScaleY);
+});
+
+
+
+function collectImageDTOs() {
+    const imageDTOs = [];
+    for (let index in uploadedImages) {
+        if (uploadedImages.hasOwnProperty(index)) {
+            const formData = new FormData();
+            formData.append('index', index);
+            formData.append('imageFile', uploadedImages[index]);
+            imageDTOs.push({ index: index, imageFile: uploadedImages[index] });
+        }
+    }
+    return imageDTOs;
+}
+    async function submitFormAjax() {
+        const form = $('#addTypeRoomForm');
+
+        // Kiểm tra tính hợp lệ của form
+        if (!validateTypeRoomForm()) {
+            return; // Dừng lại nếu form không hợp lệ
+        }
+
+
+        const formData = new FormData();
+
+        // Thu thập dữ liệu từ form
+        formData.append("TypeName", document.getElementById("TypeName").value);
+                    formData.append("NumberOfBeds", document.getElementById("NumberOfBeds").value);
+                    formData.append("NumberOfAdults", document.getElementById("NumberOfAdults").value);
+                    formData.append("NumberOfChildren", document.getElementById("NumberOfChildren").value);
+                    formData.append("Price", document.getElementById("Price").value);
+        formData.append("Description", document.getElementById("Description").value);
+
+        // Thu thập danh sách dịch vụ từ Select2
+                    const selectedServices = $('#Services').val(); 
+                    if (selectedServices) {
+                        selectedServices.forEach(serviceId => {
+                            formData.append("ServiceIds", serviceId); 
+                        });
+                        console.log("Selected Services: ", selectedServices);
+                    }
+
+        // Thu thập các ảnh từ collectImageDTOs
+                    const imageDTOs = collectImageDTOs();
+                    imageDTOs.forEach((imageDTO, index) => {
+                        formData.append(`Images[${index}][index]`, imageDTO.index);
+                        formData.append(`Images[${index}][imageFile]`, imageDTO.imageFile);
+                    });
+            
+        // AJAX request
+        $.ajax({
+            url: '/Manager/TypeRoom/Create?handler=Post', // URL tới phương thức xử lý
+            type: 'POST',
+            data: formData,
+            contentType: false,
+            processData: false,
+            headers: {
+                'RequestVerificationToken': $('input[name="__RequestVerificationToken"]').val()
+            },
+            beforeSend: function () {
+                Swal.fire({
+                    title: 'Processing',
+                    text: 'Saving type room details...',
+                    allowOutsideClick: false,
+                    showConfirmButton: false,
+                    willOpen: () => {
+                        Swal.showLoading();
+                    }
+                });
+            },
+            success: function (response) {
+                if (response.success) {
+                    Swal.fire("Success", "Type room information saved successfully!", "success")
+                        .then(() => window.location.href = '/Manager/TypeRoom/List'); // Redirect to rooms list
+                }
+            },
+            error: function (xhr, status, error) {
+                Swal.fire("Error", "There was an error saving the type room information.", "error");
+                console.log(xhr.responseText);
+            }
+
+        });
+       
+    }
+
+function validateTypeRoomForm() {
+    // Thiết lập validate cho form
+    $('#addTypeRoomForm').validate({
+        rules: {
+            TypeName: {
+                required: true,
+            },
+            NumberOfBeds: {
+                required: true,
+                min: 1,
+            },
+            NumberOfAdults: {
+                required: true,
+                min: 1,
+            },
+            NumberOfChildren: {
+                required: true,
+
+            },
+            Price: {
+                required: true,
+                min: 1
+            },
+            ServiceIds: {
+                required: true
+            },
+            Description: {
+                maxlength: 500
+            }
+        },
+        messages: {
+            TypeName: {
+                required: "Please enter the type room name.",
+            },
+            NumberOfBeds: {
+                required: "Please enter the number of beds.",
+                min: "Must have at least 1 bed.",
+            },
+            NumberOfAdults: {
+                required: "Please enter the number of adults.",
+                min: "Must allow at least 1 adult.",
+            },
+            NumberOfChildren: {
+                required: "Please enter the number of children.",
+            },
+            Price: {
+                required: "Please enter the room price.",
+                min: "Price must be at least 1."
+            },
+            ServiceIds: {
+                required: "Please select at least one service."
+            },
+            Description: {
+                maxlength: "Description cannot exceed 500 characters."
+            }
+        },
+        errorPlacement: function (error, element) {
+            error.addClass("text-danger");
+            if (element.prop("tagName") === "SELECT") {
+                error.insertAfter(element.next('.select2-container'));
+            } else {
+                error.insertAfter(element);
+            }
+        }
+    });
+
+    // Trả về kết quả hợp lệ của form
+    return $('#addTypeRoomForm').valid();
+}
+
+
+// Hàm kiểm tra số phòng đã tồn tại qua AJAX
+function checkRoomNumberExists(roomNumber) {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url: '/Manager/Room/Create?handler=CheckRoomNumber', // URL tới phương thức kiểm tra
+            type: 'POST',
+            data: { roomNumber: roomNumber },
+            headers: {
+                'RequestVerificationToken': $('input[name="__RequestVerificationToken"]').val()
+            },
+            success: function (response) {
+                resolve(response.exists); // Trả về kết quả tồn tại của số phòng qua Promise
+            },
+            error: function () {
+                Swal.fire("Error", "The room number is existed.", "error");
+                reject();
+            }
+        });
+    });
+}
+
+function showRoomNumberError() {
+    const roomNumberField = $("#RoomNumber");
+    roomNumberField.addClass("is-invalid"); // Đánh dấu trường là không hợp lệ
+
+    // Kiểm tra xem đã có thông báo lỗi chưa, nếu chưa thì thêm vào
+    if (!$("#RoomNumber-error").length) {
+        $("<span id='RoomNumber-error' class='text-danger'>This room number already exists.</span>")
+            .insertAfter(roomNumberField);
+    }
+}
+
