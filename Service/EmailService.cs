825d67f35bc1057a5f9e607aad9e352872f625ff
DangCH <dangchhe176896@fpt.edu.vn>
2024-10-30 01:40:11 +0700

    demo sendmail

---
+++ Service/EmailService.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace Bookings_Hotel.Service
+{
+    public class EmailService
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string content)
+        {
+            var smtpSettings = _configuration.GetSection("Smtp");
+            var fromAddress = new MailAddress(smtpSettings["User"]); // Sử dụng `User` làm địa chỉ người gửi
+            var toAddress = new MailAddress(email);
+
+            using var smtp = new SmtpClient
+            {
+                Host = smtpSettings["Server"],
+                Port = int.Parse(smtpSettings["Port"]),
+                EnableSsl = true,
+                Credentials = new NetworkCredential(smtpSettings["User"], smtpSettings["Pass"])
+            };
+
+            using var mailMessage = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = content,
+                IsBodyHtml = true // Nếu cần gửi HTML email, để `true`
+            };
+
+            await smtp.SendMailAsync(mailMessage);
+        }
+    }
+}
